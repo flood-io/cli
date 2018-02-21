4080fd76cffd818287b95945822c0b5f67709430
Lachie Cox <lachiec@gmail.com>
2018-02-22 09:54:02 +1100

    integrate bugsnag by recovering panics

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"os"
 
+	bugsnag "github.com/bugsnag/bugsnag-go"
 	"github.com/flood-io/cli/cmd"
 )
 
 var version string = "dev"
 var commit string = "dev"
 var date string = "today"
+var bugsnagAPIKey string
 
 func init() {
 	cmd.Version = version
@@ -18,8 +20,39 @@ func init() {
 }
 
 func main() {
+	// logger := log.New(os.Stderr, "", log.Lshortfile)
+
+	bugsnag.Configure(bugsnag.Configuration{
+		// Your Bugsnag project API key
+		APIKey: bugsnagAPIKey,
+		// The development stage of your application build, like "alpha" or
+		// "production"
+		ReleaseStage: "production",
+		// The import paths for the Go packages containing your source files
+		ProjectPackages: []string{"main", "github.com/flood-io/cli*", "github.com/flood-io/go-wrenches*"},
+
+		Logger: nil,
+	})
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("An internal error occurred, sorry!")
+			fmt.Println("We've been notified of the problem.")
+			fmt.Println("Feel free to try again soon, or if your problem persists please")
+			fmt.Println("contact Flood support at support@flood.io")
+
+			if os.Getenv("FLOOD_DEBUG") != "" {
+				fmt.Println("flood debug mode on, re-panicing:")
+				panic(r)
+			} else {
+				os.Exit(1)
+			}
+		}
+	}()
+	defer bugsnag.AutoNotify()
+
 	if err := cmd.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
-		os.Exit(-1)
+		os.Exit(1)
 	}
 }
